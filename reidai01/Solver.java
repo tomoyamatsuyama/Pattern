e0248ede0eb3e5f314251d0155592cb01a8745e4
tomoyamatsuyama <m.tomoya0221@gmail.com>
2018-06-14 19:52:40 +0900

    IOError

--- reidai01/Solver.java
+++ reidai01/Solver.java
@@ -1,19 +1,50 @@
+import java.util.*;
+
+import java.math.*;
+
 public class Solver {
 
 	public static String[] answer() {
 //		Class = "A","B" or "C" 
 
-//		showdata();
+		double setA = -0.040474694;
+		double setB = 0.0084862875342282;
+		double setC = 0.0344168757355263;
+
+		// showdata();
 		String[] kaitou = new String[Data.numdata];
 		for(int i=0; i<Data.numdata; i++){
-			double x = Data.zokusei[i][0];
-			if (x < (-1.0 + 1.0/3.0) ) {
-				kaitou[i] = "A";
-			} else if (x < (-1.0 + 2.0/3.0) ) {
-				kaitou[i] = "B";
-			} else {
-				kaitou[i] = "C";
-			}
+			Double one = Data.zokusei[i][0];
+			Double two = Data.zokusei[i][1];
+			Double three = Data.zokusei[i][2];
+			Double four = Data.zokusei[i][3];
+			Double five = Data.zokusei[i][4];
+
+			Double ave = (one + two + three + four + five) / 5;
+
+			Double diffA = Math.abs(ave - setA);
+			Double diffC = Math.abs(ave - setB);
+			Double diffB = Math.abs(ave - setC);
+
+			HashMap<Double, String> diffMap = new HashMap<>();
+			diffMap.put(diffA, "A");
+			diffMap.put(diffB, "B");
+			diffMap.put(diffC, "C");
+
+			Object[] mapKey = diffMap.keySet().toArray();
+			Arrays.sort(mapKey);
+
+			Object key = mapKey[0];
+			String value = diffMap.get(key);
+			System.out.println(value);
+
+			// if (x < (-1.0 + 1.0/3.0) ) {
+			// 	kaitou[i] = "A";
+			// } else if (x < (-1.0 + 2.0/3.0) ) {
+			// 	kaitou[i] = "B";
+			// } else {
+			// 	kaitou[i] = "C";
+			// }
 		}
 		return kaitou;
 	}
